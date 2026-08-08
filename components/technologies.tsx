9d23000ad8c9921116d6ec077ace51e6a4d47ec4
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2026-08-08 22:35:20 +0700

    Replaced Docker with Node

--- components/technologies.tsx
+++ components/technologies.tsx
@@ -5,7 +5,7 @@ import {
   SiNextdotjs,
   SiTailwindcss,
   SiTypescript,
-  SiDocker,
+  SiNodedotjs,
 } from "react-icons/si";
 
 const tools = [
@@ -19,7 +19,7 @@ const tools = [
   },
   { id: 4, name: "Tailwind", icon: SiTailwindcss, color: "text-sky-400" },
   { id: 5, name: "React Native", icon: SiReact, color: "text-blue-400" },
-  { id: 6, name: "Docker", icon: SiDocker, color: "text-blue-600" },
+  { id: 6, name: "Node.js", icon: SiNodedotjs, color: "text-blue-600" },
 ];
 
 const Technologies: React.FC = () => {
